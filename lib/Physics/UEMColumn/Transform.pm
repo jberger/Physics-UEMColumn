8e3a77ef125e0aaf836172a917366d44fb9ebcc7
Joel Berger <joel.a.berger@gmail.com>
2011-09-28 10:20:25 -0500

    Setup basic transform class structure

---
+++ lib/Physics/UEMColumn/Transform.pm
@@ -0,0 +1,46 @@
+use MooseX::Declare;
+
+class Physics::UEMColumn::Transform {
+
+  use Physics::UEMColumn::Auxiliary ':all';
+
+  has 'active' => ( isa => 'Bool', is => 'ro', default => 0 );
+
+  method time (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method space (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method velocity (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method sigma (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method eta (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method gamma (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method mass (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method charge (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+  method permitivity (Num $input, Num $direction = 1) {
+    return $input;
+  }
+
+
+}
