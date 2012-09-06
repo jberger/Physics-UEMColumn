f5bcc2f86683fdf2b7e71a107e0ceb48596e0b39
Joel Berger <joel.a.berger@gmail.com>
2012-09-06 10:27:29 -0500

    remove MXD from Laser.pm

--- lib/Physics/UEMColumn/Laser.pm
+++ lib/Physics/UEMColumn/Laser.pm
@@ -1,12 +1,15 @@
-use MooseX::Declare;
-use Method::Signatures::Modifiers;
+package Physics::UEMColumn::Laser;
 
-class Physics::UEMColumn::Laser {
+use Moose;
+use namespace::autoclean;
 
-  use MooseX::Types::NumUnit qw/num_of_unit/;
+use MooseX::Types::NumUnit qw/num_of_unit/;
 
-  has 'energy'   => ( isa => num_of_unit('J'), is => 'ro', required => 1 );
-  has 'width'    => ( isa => num_of_unit('m'), is => 'rw', required => 1 );
-  has 'duration' => ( isa => num_of_unit('s'), is => 'rw', required => 1 );
+has 'energy'   => ( isa => num_of_unit('J'), is => 'ro', required => 1 );
+has 'width'    => ( isa => num_of_unit('m'), is => 'rw', required => 1 );
+has 'duration' => ( isa => num_of_unit('s'), is => 'rw', required => 1 );
 
-}
+__PACKAGE__->meta->make_immutable;
+
+1;
+
