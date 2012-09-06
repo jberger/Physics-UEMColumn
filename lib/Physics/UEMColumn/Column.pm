52713d480d8e65365435e2ce41804593aab49cda
Joel Berger <joel.a.berger@gmail.com>
2012-09-06 10:25:11 -0500

    remove MXD from Column.pm

--- lib/Physics/UEMColumn/Column.pm
+++ lib/Physics/UEMColumn/Column.pm
@@ -1,36 +1,40 @@
-use MooseX::Declare;
-use Method::Signatures::Modifiers;
+package Physics::UEMColumn::Column;
 
-class Physics::UEMColumn::Column {
+use Moose;
+use namespace::autoclean;
 
-  use MooseX::Types::NumUnit qw/num_of_unit/;
+use Method::Signatures;
+use MooseX::Types::NumUnit qw/num_of_unit/;
 
-  has laser => ( isa => 'Physics::UEMColumn::Laser', is => 'ro', predicate => 'has_laser' );
-  has accelerator => ( isa => 'Physics::UEMColumn::Accelerator', is => 'ro', predicate => 'has_accelerator' );
-  has photocathode => ( isa => 'Physics::UEMColumn::Photocathode', is => 'ro', predicate => 'has_photocathode' );
+has laser => ( isa => 'Physics::UEMColumn::Laser', is => 'ro', predicate => 'has_laser' );
+has accelerator => ( isa => 'Physics::UEMColumn::Accelerator', is => 'ro', predicate => 'has_accelerator' );
+has photocathode => ( isa => 'Physics::UEMColumn::Photocathode', is => 'ro', predicate => 'has_photocathode' );
 
-  has elements => ( 
-    traits => ['Array'],
-    isa => 'ArrayRef[Physics::UEMColumn::Element]',
-    is => 'rw',
-    handles => {
-      add_element  => 'push',
-    },
-    default => sub{ [] },
-  );
+has elements => ( 
+  traits => ['Array'],
+  isa => 'ArrayRef[Physics::UEMColumn::Element]',
+  is => 'rw',
+  handles => {
+    add_element  => 'push',
+  },
+  default => sub{ [] },
+);
 
-  has 'length' => ( isa => num_of_unit('m'), is => 'rw', required => 1 );
+has 'length' => ( isa => num_of_unit('m'), is => 'rw', required => 1 );
 
-  method BUILD (Item $params) {
-    if ($self->has_accelerator && $self->has_photocathode) {
-      $self->add_element( $self->accelerator );
-      $self->photocathode->column( $self );
-    }
-  } 
-
-  method can_make_pulse () {
-    return $self->has_laser && $self->has_accelerator && $self->has_photocathode;
+method BUILD (Item $params) {
+  if ($self->has_accelerator && $self->has_photocathode) {
+    $self->add_element( $self->accelerator );
+    $self->photocathode->column( $self );
   }
+} 
 
+method can_make_pulse () {
+  return $self->has_laser && $self->has_accelerator && $self->has_photocathode;
 }
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
 
