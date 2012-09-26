jberger/Physics-UEMColumn

commit 1d6ba8e95533a72ce7f76b485d46caccb6e1110d

begin Photocathode documentation

lib/Physics/UEMColumn/Photocathode.pm
@@ -1,5 +1,25 @@
 package Physics::UEMColumn::Photocathode;
 
+package Physics::UEMColumn::Pulse;
+
+=head1 NAME
+
+Physics::UEMColumn::Pulse - Class representing a pulse for the Physics::UEMColumn simulation
+
+=head1 SYNOPSIS
+
+  use strict;
+  use warnings;
+
+  use Physics::UEMColumn alias => ':standard';
+
+  my $photocathode = Photocathode->new(
+    energy_fermi => '5.3 eV',
+    work_function => '4.25 eV',
+  );
+
+=cut
+
 use Moose;
 use namespace::autoclean;
 

commit 41fa0f6fb7b85849ba3daaa304e1adc33f2c8046

fixed bug introduced in 1d6ba8e95533a72ce7f76b485d46caccb6e1110d

lib/Physics/UEMColumn/Photocathode.pm
@@ -1,10 +1,8 @@
 package Physics::UEMColumn::Photocathode;
 
-package Physics::UEMColumn::Pulse;
-
 =head1 NAME
 
-Physics::UEMColumn::Pulse - Class representing a pulse for the Physics::UEMColumn simulation
+Physics::UEMColumn::Photocathode - Class representing a photocathode for the Physics::UEMColumn simulation
 
 =head1 SYNOPSIS
 